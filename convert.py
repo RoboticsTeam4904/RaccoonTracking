c7fbc2d42780f906dafa0801e49731bb3897699f
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2018-01-15 13:50:17 -0800

    convert.py returns txt files

--- convert.py
+++ convert.py
@@ -3,8 +3,8 @@ import xml.etree.ElementTree as ET
 import sys
 import os
 
-#takes in paths to file to convert, dir with corresponding img inside, and xml dir to write new file in
-def convert(xmlFilePath, imgFolder, newXmlFolder): 
+#takes in path to xml file and new folder to put .txt in
+def convertToTxt(xmlFilePath, newTxtFolder):
 	tree = ET.parse(xmlFilePath)
 	xs = []
 	ys = []
@@ -12,11 +12,17 @@ def convert(xmlFilePath, imgFolder, newXmlFolder):
 	imgWidth = 0
 	imgHeight = 0
 
-	for a in tree.iter("polygon"): #finding x and y in formatting
-	    x = a.find('pt').find('x').text
-	    y = a.find('pt').find('y').text
-	    xs.append(x)
-	    ys.append(y)
+	#finds x and y coordinates of corners of every polygon and adds them to list xs and ys
+	for polygon in tree.iter("polygon"):
+		polygonXs = []
+		polygonYs = []
+		for pt in polygon.iter('pt'):
+			x = pt.find('x').text
+			y = pt.find('y').text
+			polygonXs.append(x)
+			polygonYs.append(y)
+		xs.append(polygonXs)
+		ys.append(polygonYs)
 
 	for a in tree.iter("imagesize"): #finding img width and height
 	    x = a.find('nrows').text
@@ -24,48 +30,30 @@ def convert(xmlFilePath, imgFolder, newXmlFolder):
 	    imgWidth = x
 	    imgHeight = y
 
-	try:
-		width = str(abs(int(xs[1]) - int(xs[0])))
-		height = str(abs(int(ys[1]) - int(ys[0])))
-	except IndexError:
-		width = 0
-		height = 0
+	txtFormat = ""
 
-	#if there are no xs or ys, set the min and max to 0
-	if (xs != []):
-		minx = min(xs)
-		maxx = max(xs)
-	else:
-		minx = 0
-		maxx = 0
-	if (ys !=[]):
-		miny = min(ys)
-		maxy = max(ys)
-	else:
-		miny = 0
-		maxy = 0
-
-
-	#Finding corresponding img for xml file
-	head, tail = os.path.split(xmlFilePath)
-	filename = os.path.basename(imgFolder).split('.')[0] + ".jpg"
-	path = os.path.join(head, filename)
-	name = "cube"
-
-	#output xml format
-	updatedXML = "<annotationverified='yes'><folder>images</folder><filename>"+filename+"</filename><path>"+path+"</path><source><database>Unknown</database></source><size><width>"+imgWidth+"</width><height>"+imgHeight+"</height><depth>3</depth></size><segmented>0</segmented><object><name>"+name+"</name><pose>Unspecified</pose><truncated>0</truncated><difficult>0</difficult><bndbox><xmin>"+str(minx)+"</xmin><ymin>"+str(miny)+"</ymin><xmax>"+str(maxx)+"</xmax><ymax>"+str(maxy)+"</ymax></bndbox></object></annotation>"
+	#formatting txt
+	for xTuple, yTuple in zip(xs, ys):
+		try:
+			width = abs(int(xTuple[0]) - int(xTuple[1]))
+			height = abs(int(yTuple[0]) - int(yTuple[2]))
+		except:
+			width = 0
+			height = 0
+		xCenter = width / 2
+		yCenter = height / 2
+		txtFormat += "0 " + str(xCenter) + " " + str(yCenter) + " " + str(width) + " " + str(height) + "\n"
 	
-	#writing xml onto new xml file
-	newPath = newXmlFolder + "/" + os.path.basename(xmlFilePath)
+	#writing xml onto txt file
+	newPath = newTxtFolder + "/" + os.path.basename(xmlFilePath).split('.')[0] + ".txt"
 	newFile = open(newPath, "w")
-	newFile.write(updatedXML)
+	newFile.write(txtFormat)
 	newFile.close()
 
-#converts an entire folder
-def convertFolder(xmlFolder=sys.argv[1], imgFolder=sys.argv[2], newXmlFolder=sys.argv[3]):
+#converts entire folder to txt
+def convertXMLToText(xmlFolder=sys.argv[1],newTxtFolder=sys.argv[2]):
 	for fn in os.listdir(xmlFolder):
 		qualifiedName = xmlFolder + "/" + fn
-		convert(qualifiedName, imgFolder, newXmlFolder)
+		convertToTxt(qualifiedName,newTxtFolder)
 
-convertFolder()
-#convertFolder("/Users/niksure/Documents/workspace/RaccoonTracking/data/annotations", "/Users/niksure/Documents/workspace/RaccoonTracking/data/images", "/Users/niksure/Documents/workspace/RaccoonTracking/data/updatedXMLs")
+convertXMLToText()
